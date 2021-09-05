e5412e4b45ac979513a660c2a05bac75e95d3cbe
YHMunna <yeakubhossainmunna2017@gmail.com>
2021-09-05 16:02:59 +0600

    wow

--- shopping.js
+++ shopping.js
@@ -1,4 +1,12 @@
 const addProduct = () => {
-    const productName = document.getElementById('productInputField').value;
+    const productName = document.getElementById('productInputField');
+    const name = productName.value;
+    displayProduct(name)
     productName.value = '';
+}
+const displayProduct = name => {
+    const ul = document.getElementById('productslist');
+    const li = document.createElement('li');
+    li.innerText = name;
+    ul.appendChild(li);
 }
